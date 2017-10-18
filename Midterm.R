2a8a3a96d263ef07e4d567d13e84baa742389b44
ajaegge <andrea.jaegge@gmail.com>
2017-10-18 12:19:30 -0500

    Macrozoo correct lat/long format

--- Midterm.R
+++ Midterm.R
@@ -21,3 +21,13 @@ elninoyearzoo <- subset(M, YYYY >= 1997 & YYYY <= 1998)
 elninoyearzoo$Date <- str_c(elninoyearzoo$YYYY,"-",elninoyearzoo$MM,"-",elninoyearzoo$DD)
 as.Date(elninoyearzoo$Date, "%Y-%m-%d")
 elninomonthzoo <- subset(elninoyearzoo, Date >= "1997-07-01" & Date <= "1998-06-22")
+elninomonthzoo$ZooDateTime <- str_c(elninomonthzoo$Date, " ", elninomonthzoo$tow_time)
+
+#edit macrozoo lat/long format
+install.packages("measurements")
+MacroZoo <- elninomonthzoo
+MacroZoo$LAT <- str_c(MacroZoo$lat_deg, " ", MacroZoo$lat_min)
+MacroZoo$LATdec = measurements::conv_unit(MacroZoo$LAT, from = 'deg_dec_min', to = 'dec_deg')
+MacroZoo$LONG <- str_c(MacroZoo$lon_deg, " ", MacroZoo$lon_min)
+MacroZoo$LONGdec = measurements::conv_unit(MacroZoo$LONG, from = 'deg_dec_min', to = 'dec_deg')
+MacroZoo$LONGdec = paste("-", MacroZoo$LONGdec, sep="")
